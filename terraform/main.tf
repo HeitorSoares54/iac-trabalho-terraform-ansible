cc4d296d4de1b94d518490bb35bc36b8b10c6b5a
HeitorSoares54 <heitordossantossoares@gmail.com>
2025-11-07 17:48:35 -0300

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,119 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name                = var.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-trabalho"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  depends_on = [ azurerm_resource_group.rg ]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-trabalho"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on = [ azurerm_resource_group.rg ]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-trabalho-vm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  depends_on = [ azurerm_resource_group.rg ]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-trabalho"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  depends_on = [ azurerm_resource_group.rg ]
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-trabalho-vm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  depends_on = [ azurerm_resource_group.rg ]
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "vm-trabalho"
+  resource_group_name   = var.resource_group_name
+  depends_on = [ azurerm_resource_group.rg ]
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+}
